d81143dd81c8013c7503ab2376b346f57b63d77b
Cancer Institute Lucia Conde <regmr01@login06.data.legion.ucl.ac.uk>
2017-05-09 12:16:16 +0100

    Added original cytofkit heatmap

--- cytofpipe/cytofpipe.R
+++ cytofpipe/cytofpipe.R
@@ -210,6 +210,11 @@ if(!is.null(clusterData) && length(clusterData) > 0){
 		
 			## cluster mean 
 			cluster_mean <- cytof_clusterStat(data= exprs_cluster_sample, cluster = "cluster", statMethod = "mean")
+
+			pdf(paste0(outputdir,"/",projectName, "_",methodj, "_cluster_mean_heatmap.pdf"))
+			cytof_heatmap(cluster_mean, paste(projectName, methodj, "\ncluster mean", sep = " "))
+			dev.off()
+
 			pdf(paste0(outputdir,"/",projectName, "_",methodj, "_cluster_mean_heatmap_scaled.pdf"))
 			cytof_heatmap(cluster_mean, scaleMethod="column", paste(projectName, methodj, "\ncluster mean (scaled)", sep = " "))
 			dev.off()
