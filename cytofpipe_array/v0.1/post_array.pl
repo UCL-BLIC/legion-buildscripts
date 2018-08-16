796918a3a4b7c3b951a70026d7cf11d938d3ec60
Cancer Institute Lucia Conde <regmr01@login06.data.legion.ucl.ac.uk>
2018-08-16 14:56:24 +0100

    Add original run to post_array merging

--- cytofpipe_array/v0.1/post_array.pl
+++ cytofpipe_array/v0.1/post_array.pl
@@ -35,6 +35,13 @@ for (my $i=2; $i<=$iter; $i++){
 
 my @clust=keys(%clusteringmethod);
 my $clust=join(",",@clust);
+foreach my $clm(@clust){ 
+	foreach my $type("median_data", "cell_percentage"){
+		system("cp $ENV{PWD}/${out}/$clm/cytofpipe_${clm}_cluster_${type}.csv $ENV{PWD}/${out}_array_files/$clm/$type/cytofpipe_${clm}_cluster_${type}_1.csv");
+		system("perl -i -pe \'s/\"\"/\"clusterID\"/ if 1\' $ENV{PWD}/${out}_array_files/$clm/$type/cytofpipe_${clm}_cluster_${type}_1.csv"); # header of first column was empty, add header
+		system("perl -i -pe \'s/\",/_1\",/ if \$\. > 1\'  $ENV{PWD}/${out}_array_files/$clm/$type/cytofpipe_${clm}_cluster_${type}_1.csv"); # add iteration number to cluster id
+	}
+}
 
 system("OUT=$ENV{PWD}/$out ITER=$iter CLUST=$clust R CMD BATCH ${cytofpipe_array_home}/merge_files.R");
 system("rm -rf merge_files.Rout");
